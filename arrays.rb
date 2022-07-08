7fd3b3f6a67d1a9c4426e75cfac025ef13d1663d
Sean Culliton <smculliton@gmail.com>
2022-07-08 15:51:15 -0600

    Add arrays

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+restaurants = ["Safta", "Suvipa", "McDonald's", "Taco de Mexico"]
+id_numbers = [10, 15, 16, 19]
+food_prices = [20.10, 16.99, 8.99, 12.50]
+open_on_sunday = [true, true, false, false]
